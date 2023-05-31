e7f16953bfcc6ed4d6d2dee016fcde505aafd58a
pasi1337 <43679886+byayex@users.noreply.github.com>
2023-05-31 17:15:22 +0200

    Updated some things.

--- runthis.py
+++ runthis.py
@@ -7,26 +7,22 @@ y_min, y_max = 905, 930
 input_x_min, input_x_max = 1461, 1510
 input_y_min, input_y_max = 950, 960
 
-# Wait so we can open the menu
-time.sleep(5)
-
 while True:
-    time.sleep(random.randint(1,4))
+    time.sleep(random.randint(2,3))
     # Still in the shop check
-    check_shop = pyautogui.pixel(1466,920)
+    check_shop = pyautogui.pixel(1466,920) # Checks if "buy" button is green -> restocked
     if check_shop == (115, 141, 69) or check_shop == (110, 135, 66):
         current_time = datetime.datetime.now()
         print('------------------')
-        print('Current Time: %s:%s:%s' % (current_time.hour, current_time.minute, current_time.second))
         print('Detected new Stock -> Start Selling now!')
-        time.sleep(random.randint(1,3))
+        print('Current Time: %s:%s:%s' % (current_time.hour, current_time.minute, current_time.second))
         # Select the random of the input, to put mouse to
         input_x, input_y = random.randint(input_x_min, input_x_max), random.randint(input_y_min, input_y_max)
         # Move to the Input Field
         pyautogui.moveTo(input_x, input_y, random.random(), pyautogui.easeOutQuad)
         # Click it
         pyautogui.leftClick()
-        # Input high amount to sell, so it sells the max amount, wait 0.1-0.3 seconds per input
+        # Input high amount to sell, so it sells the max amount, wait 0.1-0.2 seconds per input
         pyautogui.press(str(random.randint(1,9)))
         time.sleep(random.randint(1,2)/10)
         pyautogui.press(str(random.randint(1,9)))
@@ -41,11 +37,11 @@ while True:
         print("Sold successfully!")
         pyautogui.moveTo(random.randint(250, 1150), random.randint(133,920), random.random(), pyautogui.easeOutQuad)
         print("Moving back to idle position")
-        print('------------------')
+
 
     # Move mouse sometimes
-    rand = random.randint(0,10)
-    if rand == 1:
+    rand = random.randint(0,25)
+    if rand > 20:
         print("Moving mouse randomly, to prevent detection!")
         pyautogui.moveTo(random.randint(250, 1150), random.randint(133,920), random.random(), pyautogui.easeOutQuad)
     if rand == 2:
@@ -61,6 +57,7 @@ while True:
         pyautogui.keyDown(key1)
         time.sleep(randomtime)
         pyautogui.keyUp(key1)
+        time.sleep(randomtime/2)
         pyautogui.keyDown(key2)
         time.sleep(randomtime)
         pyautogui.keyUp(key2)
